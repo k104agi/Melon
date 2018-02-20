bff97e087420778a513c9a299e8ed33c3fb6518b
kjs <k104agi@gmail.com>
2018-02-20 10:58:20 +0900

    ppost method에서 get으로 바꾸는 중에 커밋

--- django/song/views.py
+++ django/song/views.py
@@ -1,9 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-from album.models import Album
-from artist.models import Artist
-from song.models import Song
+from .models import Song
 from django.db.models import Q
 
 
@@ -65,6 +61,25 @@ def song_search(request):
                 Q(title__contains=keyword)
             ).distinct()
             context['songs'] = songs
+
+        # Song과 연결된 Artist의 name에 keyword가 포함되는 경우
+        songs_from_artists = Song.objects.filter(
+            album__artists__name__contains=keyword
+        )
+        context['songs_from_artists'] = songs_from_artists
+
+        # album name에 keyword
+        songs_from_albums = Song.objects.filter(
+            album__title__contains=keyword
+        )
+        context['songs_from_albums'] = songs_from_albums
+
+        # title name에 keyword
+        songs_from_title = Song.objects.filter(
+            title__contains=keyword
+        )
+        context['songs_from_titles'] = songs_from_title
+
         # 만약 method가 POST였다면 context에 'songs'가 채워진 상태
         # GET이면 빈 상태로 render 실행
     return render(request, 'song/song_search.html', context)
